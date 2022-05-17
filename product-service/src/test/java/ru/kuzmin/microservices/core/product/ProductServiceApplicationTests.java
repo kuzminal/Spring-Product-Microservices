3f262a6c56d37a5be54b5ff2352fafb6d067587a
Aleksey Kuzmin <kuzmin.al35@gmail.com>
2022-05-17 18:16:31 +0300

    Инициализация проекта и каркас сервисов

---
+++ product-service/src/test/java/ru/kuzmin/microservices/core/product/ProductServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuzmin.microservices.core.product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ recommendation-service/src/test/java/ru/kuzmin/microservices/core/recommendation/RecommendationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.kuzmin.microservices.core.recommendation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecommendationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
